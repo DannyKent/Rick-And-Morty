93eca28d3d6cea91363bf7bc3c88abe33579f696
Danny Kent <vidaniel057@gmail.com>
2023-05-29 21:04:11 -0500

    Nuevos Cambios

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,15 +1,14 @@
 import style from "./SearchBar.module.css";
 
 export default function SearchBar(props) {
-  return (
-    <div className={style.SearchContainer}>
-     <input type="search" placeholder="Ingresa tu Busqueda" />
-      <div>
-         <img src={'../assets/logo.png'} alt="" />
+  return <div className={style.SearchContainer}>
+      <div className="SearchContainer">
+        <img src={"../assets/logo.png"} alt=" Logo" />
       </div>
+
+      <input type="search" placeholder="Ingresa tu Busqueda" />
       <div className={style.buttonSearch}>
-         <button onClick={props.onSearch}>Agregar</button>
+        <button onClick={props.onSearch}>Agregar</button>
       </div>
-    </div>
-  );
+    </div>;
 }
